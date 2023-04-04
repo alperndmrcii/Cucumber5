75e5ed22ca59bfc6b6fd20a702ca3c2857c976c7
Alperen Demirci <alperndmrcii@gmail.com>
2023-04-04 21:24:41 +0300

    Apache Cucumber Excel Framework

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -1,10 +1,14 @@
+
 package StepDefinitions;
 
+import Utilities.ExcelUtility;
 import Utilities.GWD;
 import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
 
 public class Hooks {
 
@@ -12,11 +16,15 @@ public class Hooks {
     @After  // her senaryodan sonra çalışacak bölüm
     public void after(Scenario senaryo){
         System.out.println("Senaryo bitti");
-        if (senaryo.isFailed()){
-            TakesScreenshot ts=(TakesScreenshot)GWD.getDriver();
-            final byte[] hafizadakiHali=ts.getScreenshotAs(OutputType.BYTES);
-            senaryo.attach(hafizadakiHali,"image/png","screenshot name");
 
+        ExcelUtility.writeExcel("src/test/java/ApachePOI/resource/ScenarioStatus.xlsx",
+                senaryo, GWD.threadBrowserGet());
+
+        if (senaryo.isFailed()){
+            // extend report da çalışan kısım
+//            TakesScreenshot ts=(TakesScreenshot) GWD.getDriver();
+//            final byte[] hafizadakiHali=ts.getScreenshotAs(OutputType.BYTES);
+//            senaryo.attach(hafizadakiHali, "image/png","screenshot name");
         }
 
         GWD.quitDriver();
