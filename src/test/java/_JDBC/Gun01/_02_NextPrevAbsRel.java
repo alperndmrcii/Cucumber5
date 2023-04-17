665d321d8b1b8bc89c77c053bbc3a17586ca0919
Alperen Demirci <alperndmrcii@gmail.com>
2023-04-18 01:50:04 +0300

    JDBC _02_NextPrevAbsRel 2 Absolute Relative

--- src/test/java/_JDBC/Gun01/_02_NextPrevAbsRel.java
+++ src/test/java/_JDBC/Gun01/_02_NextPrevAbsRel.java
@@ -25,4 +25,19 @@ public class _02_NextPrevAbsRel extends JDBCParent {
         System.out.println("1 geri satır dilAdi="+ rs.getString(2));  // italian
 
     }
+    @Test
+    public void AbsoluteRelative() throws SQLException {
+        ResultSet rs= statement.executeQuery("select * from film");
+        //rs.next(); sonraki satır demek
+        //rs.previous; önceki satır demek
+        // rs.absolute(10); baştan itibaren parantez içindeki sayıya gider.
+        // rs. relative(10) kaçıncı yerdeyse o yerden parantez içindeki sayı kadar ileri gider.
+        // rs.absolute(-10) sondan 10.satır demek
+        //rs.relative(-10) bulunduğu yerden 10 satır geri demek
+        rs.absolute(10); //10.satıra git demek
+
+        System.out.println("10.satır = "+rs.getString("title"));
+        rs.relative(5); // 15 e gider çünkü bir önceki konumu 10 du
+
+    }
 }
