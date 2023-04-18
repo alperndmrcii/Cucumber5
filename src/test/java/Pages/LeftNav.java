126c05c97368ada3bf305af5b82fb09ec2e7dff3
Alperen Demirci <alperndmrcii@gmail.com>
2023-04-18 17:31:53 +0300

    JDBC DB list and framework

--- src/test/java/Pages/LeftNav.java
+++ src/test/java/Pages/LeftNav.java
@@ -5,11 +5,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LeftNav extends Parent{
+public class LeftNav extends Parent {
     // sol taraftaki menuler burda
 
     public LeftNav() {
-        PageFactory.initElements(GWD.getDriver(),this);
+        PageFactory.initElements(GWD.getDriver(), this);
     }
 
     @FindBy(xpath = "(//span[text()='Setup'])[1]")
@@ -30,28 +30,40 @@ public class LeftNav extends Parent{
     @FindBy(xpath = "(//span[text()='Fees'])[1]")
     private WebElement fees;
 
-    @FindBy(xpath="(//span[text()='Entrance Exams'])[1]")
+    @FindBy(xpath = "(//span[text()='Entrance Exams'])[1]")
     private WebElement entranceExamsOne;
 
-    @FindBy(xpath="(//span[text()='Setup'])[2]")
+    @FindBy(xpath = "(//span[text()='Setup'])[2]")
     private WebElement setupTwo;
 
-    @FindBy(xpath="(//span[text()='Entrance Exams'])[2]")
+    @FindBy(xpath = "(//span[text()='Entrance Exams'])[2]")
     private WebElement entranceExamsTwo;
+    @FindBy(xpath = "//span[text()='States']")
+    private WebElement states;
 
-    public WebElement getWebElement(String strButton){
+    public WebElement getWebElement(String strButton) {
 
-        switch (strButton)
-        {
-            case "setup" : return setup;
-            case "parameters" : return parameters;
-            case "countries" : return countries;
-            case "citizenShip" : return citizenShip;
-            case "nationalities" : return nationalities;
-            case "fees" : return fees;
-            case "entranceExamsOne" : return entranceExamsOne;
-            case "setupTwo" : return setupTwo;
-            case "entranceExamsTwo" : return entranceExamsTwo;
+        switch (strButton) {
+            case "setup":
+                return setup;
+            case "parameters":
+                return parameters;
+            case "countries":
+                return countries;
+            case "citizenShip":
+                return citizenShip;
+            case "nationalities":
+                return nationalities;
+            case "fees":
+                return fees;
+            case "entranceExamsOne":
+                return entranceExamsOne;
+            case "setupTwo":
+                return setupTwo;
+            case "entranceExamsTwo":
+                return entranceExamsTwo;
+            case "states":
+                return states;
         }
 
         return null;
