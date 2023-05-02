09ebec790edd0b24ba5fce2dbfe6ea268b9a14e8
Alperen Demirci <alperndmrcii@gmail.com>
2023-05-02 20:14:12 +0300

    rest assured

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -3,6 +3,11 @@ package Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = "@SmokeTest", features = {"src/test/java/FeatureFiles"}, glue = {"StepDefinitions"})
+@CucumberOptions
+                (tags = "@SmokeTest",
+                features = {"src/test/java/FeatureFiles"},
+                glue = {"StepDefinitions"},
+                plugin = {"html:target/site/cucumber-pretty.html"}
+        )
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
 }
