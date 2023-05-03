887ef5aa02f930317cd520d1dd13700d0eb4bc11
Alperen Demirci <alperndmrcii@gmail.com>
2023-05-03 20:01:00 +0300

    rest assured plugin

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -1,13 +1,14 @@
+
 package Runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions
-                (tags = "@SmokeTest",
-                features = {"src/test/java/FeatureFiles"},
-                glue = {"StepDefinitions"},
-                plugin = {"html:target/site/cucumber-pretty.html"}
-        )
+@CucumberOptions(
+        tags = "@SmokeTest", // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
+        features = {"src/test/java/FeatureFiles"},
+        glue = {"StepDefinitions"}
+        ,plugin= {"json:target/cucumber/cucumber.json"} // Jenkins için JSON report
+)
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
 }
